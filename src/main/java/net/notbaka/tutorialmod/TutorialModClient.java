e29f88957191c19b8d79362ac75e6345a4860623
notbaka <lildeadbaka@yandex.ru>
2023-02-17 16:42:05 +0300

    idk

---
+++ src/main/java/net/notbaka/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.notbaka.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
